9c0fa43148abc79812eb7bf441d3e2665756b508
Hoover <hujingguangsa@gmail.com>
2024-09-23 16:04:18 +0800

    fix bug
    修复从v1.9.1版本更新时SQL执行失败问题

--- src/init_sql/v1.10.0.sql
+++ src/init_sql/v1.10.0.sql
@@ -2,6 +2,12 @@
 alter table instance_account
     add db_name varchar(128) default '' not null comment '数据库名（mongodb）' after host;
 
+--先删除外键
+set @drop_fk_sql=(select concat('alter table instance_account drop foreign key ',constraint_name) from information_schema.table_constraints where constraint_type='foreign key' and table_name = 'instance_account');
+prepare stmt from @drop_fk_sql;
+execute stmt;
+drop prepare stmt;
+
 -- instance_account表调整唯一索引
 set @drop_sql=(select concat('alter table instance_account drop index ', constraint_name) from information_schema.table_constraints where table_schema=database() and table_name='instance_account' and constraint_type='UNIQUE');
 prepare stmt from @drop_sql;
@@ -10,3 +16,6 @@ drop prepare stmt;
 alter table instance_account add unique index uidx_instanceid_user_host_dbname(`instance_id`, `user`, `host`, `db_name`);
 --- 增加 ssl 支持
 ALTER TABLE sql_instance ADD is_ssl tinyint(1) DEFAULT 0  COMMENT '是否启用SSL';
+
+--重新添加外键
+alter table instance_account add constraint fk_account_sql_instance_id foreign key (instance_id) references sql_instance(id);
